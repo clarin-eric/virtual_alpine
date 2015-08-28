57cd22569add1ca1798485d15a99307744b5f5cb
Sander M <S.N.Maijers@gmail.com>
2015-08-28 15:14:13 +0200

    * Make sure apk cache gets purged afterwards.

--- provision.sh
+++ provision.sh
@@ -3,4 +3,5 @@
 # setup-apkcache '/etc/apk/cache/'
 apk --verbose --progress update --purge # --update-cache
 apk --verbose --progress upgrade --latest # --update-cache --available
+apk cache --purge clean
 # apk --verbose cache sync
